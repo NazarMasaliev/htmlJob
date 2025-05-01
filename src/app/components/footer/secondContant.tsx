f4443ef9b4d1bc926d066eeedc8ca9ff99823bef
NazarMasaliev <mamasalievnazarbek@gmail.com>
2025-05-01 13:21:42 +0600

    mtjob

--- src/app/components/footer/secondContant.tsx
+++ src/app/components/footer/secondContant.tsx
@@ -1,7 +1,7 @@
 // components/UolSubscriptionBar.tsx
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
+
 
 const UolSubscriptionBar: React.FC = () => {
   return (
